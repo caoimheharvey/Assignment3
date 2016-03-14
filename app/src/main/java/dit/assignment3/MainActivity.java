7ebdb1dbecf289f543b1fbca4575e2bce430d044
caoimheharvey <caoimhe.e.harvey@gmail.com>
2016-03-14 18:14:41 +0000

    Second Page added. Keeps crashing in Emulator

--- app/src/main/java/dit/assignment3/MainActivity.java
+++ app/src/main/java/dit/assignment3/MainActivity.java
@@ -1,7 +1,10 @@
 package dit.assignment3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button timerBtn = (Button) findViewById(R.id.timerButton);
+        Button pomodoroBtn = (Button) findViewById(R.id.pomoButton);
+
+        timerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                next();
+            }
+        });
+    }
+
+    private void next(){
+        Intent intent = new Intent(this, Timer.class);
+
+        startActivity(intent);
     }
 }
