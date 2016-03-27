8402b82ae1d27e2ef91d682d560c0399975fd710
caoimheharvey <caoimhe.e.harvey@gmail.com>
2016-03-27 20:27:56 +0100

    Undid previous commit due to parsing error

--- app/src/main/java/dit/assignment3/Timer.java
+++ app/src/main/java/dit/assignment3/Timer.java
@@ -9,10 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 import java.sql.Time;
 import java.util.concurrent.TimeUnit;
 
@@ -21,7 +17,6 @@ public class Timer extends AppCompatActivity {
     EditText hoursIn, minIn;
     Button start, stop;
     TextView textViewTime;
-    int minutes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +24,15 @@ public class Timer extends AppCompatActivity {
         setContentView(R.layout.activity_timer);
 
         //initiallizing declared variables
-        hoursIn = (EditText) findViewById(R.id.hoursET);
-        minIn = (EditText) findViewById(R.id.minET);
+       // hoursIn = (EditText) findViewById(R.id.hoursET);
+        //minIn = (EditText) findViewById(R.id.minET);
         start = (Button) findViewById(R.id.startButton);
         stop = (Button) findViewById(R.id.stopButton);
         textViewTime = (TextView) findViewById(R.id.timeDisp);
 
 
-        minutes = Integer.parseInt(String.valueOf(minIn)) * 1000;
 
-        textViewTime.setText("00:%02d:00", minIn);
+        textViewTime.setText("00:00:30");
 
         final CounterClass timer = new CounterClass(30000, 1000);
         start.setOnClickListener(new View.OnClickListener() {
@@ -57,53 +51,11 @@ public class Timer extends AppCompatActivity {
         });
 
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        //client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 
     }
 
 
-    //@Override
-    /*public void onStart() {
-        super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Timer", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://dit.assignment3/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Timer Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://dit.assignment3/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
-*/
     public class CounterClass extends CountDownTimer {
 
         /**
