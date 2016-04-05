3a6eea8cb6fb91c282977fe942f367bb2b7b1513
caoimheharvey <caoimhe.e.harvey@gmail.com>
2016-04-05 18:28:25 +0100

    comments added

--- app/src/main/java/dit/assignment3/Timer.java
+++ app/src/main/java/dit/assignment3/Timer.java
@@ -21,6 +21,20 @@ import android.widget.Toast;
 import java.sql.Time;
 import java.util.concurrent.TimeUnit;
 
+
+// NEW FOUND BUGS: 5 APR 2016
+/*
+ * stop alarm button doesn't disappear when start button is pressed after timer finishes
+ * notification causes app to crash
+ * the display of countdown when number greater than 1 is entered in minutes
+ * main_activity not used
+ *
+ * TODO: start working on the GUI
+ * TODO: fix all bugs and get a popup notification working
+ * TODO: have app run in OS background
+ * TODO: find ways to divide code into multiple classes for simplicity
+ */
+
 public class Timer extends AppCompatActivity {
 
     //declaring all items on page in class
