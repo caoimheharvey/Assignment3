d4aa99fcde3909e7c758fc64ed2dc0724b8b80e3
caoimheharvey <caoimhe.e.harvey@gmail.com>
2016-04-10 22:38:12 +0100

    changing UI and class functionality

--- app/src/main/java/dit/assignment3/MainActivity.java
+++ app/src/main/java/dit/assignment3/MainActivity.java
@@ -1,105 +1,93 @@
 package dit.assignment3;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.CountDownTimer;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TableLayout;
-import android.widget.TextView;
+import android.widget.Switch;
 import android.widget.Toast;
 
-import java.sql.Time;
-import java.util.concurrent.TimeUnit;
-
-
-// NEW FOUND BUGS: 5 APR 2016
+// NEW FOUND BUGS: 10 APR 2016
 /*
- * stop alarm button doesn't disappear when start button is pressed after timer finishes
- * notification causes app to crash
- * the display of countdown when number greater than 1 is entered in minutes
- * main_activity not used
- *
  * TODO: start working on the GUI
  * TODO: fix all bugs and get a popup notification working
  * TODO: have app run in OS background
  * TODO: find ways to divide code into multiple classes for simplicity
+ * TODO: Complete new UI. Start button is an intent to get to the Timer page where the actual countdown occurs
  */
 
 public class MainActivity extends AppCompatActivity {
 
-    //declaring all items on page in class
-    //sound player
-    MediaPlayer alarmSound;
+    //small button minutes
+    Button ten, twenty, thirty, sixty;
 
-    //data from XML file
-    EditText hoursIn, minIn;
-    Button start, stop, stopSoundBtn;
-    TextView textViewTime;
-    int totalTime;
-    int inHr, inMin;
-    int hoursMs, minMs;
-    TableLayout table;
+    Button startBtn, stopBtn;
+    EditText minIn, hoursIn;
+    int timeInt;
+    int minMs, hoursMs;
 
-    //notification
-    public NotificationCompat.Builder mBuilder;
-    private Intent resultAction;
-    PendingIntent resultPendingIntent;
+    //additional variables to make timer work
+    long totalTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timer);
+        setContentView(R.layout.activity_main);
 
-        //intent to act on the notification to leave it null
-        resultAction = new Intent(this, Timer.class);
+        //finding by ID
+        ten = (Button) findViewById(R.id.tenMin);
+        twenty = (Button) findViewById(R.id.twentyMin);
+        thirty = (Button) findViewById(R.id.thirtyMin);
+        sixty = (Button) findViewById(R.id.sixtyMin);
 
-        //pendingIntent.getActivity(Context, int, Intent, int)
-        resultPendingIntent = PendingIntent.getActivity(this, 0, resultAction, PendingIntent.FLAG_UPDATE_CURRENT);
+        startBtn = (Button) findViewById(R.id.startButton);
+        stopBtn = (Button) findViewById(R.id.stopButton);
 
-        //notification
-        mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.drawable.small_icon)
-                .setContentTitle("TimerApp").setContentText("Your Timer is done!");
+        hoursIn = (EditText) findViewById(R.id.hoursInput);
+        minIn = (EditText) findViewById(R.id.minsInput);
 
-        mBuilder.setContentIntent(resultPendingIntent);
+        //Parsing to correct value
 
-        //playing the alarm sound
-        alarmSound = MediaPlayer.create(this, R.raw.alarm_noise);
+        //changing times through smaller buttons
+        ten.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                minIn.setText("10");
+            }
+        });
+        twenty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                minIn.setText("20");
+            }
+        });
+        thirty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                minIn.setText("30");
+            }
+        });
+        sixty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hoursIn.setText("01");
+                minIn.setText("00");
+            }
+        });
 
-        //initiallizing declared variables
-        hoursIn = (EditText) findViewById(R.id.hoursET);
-        minIn = (EditText) findViewById(R.id.minET);
-        start = (Button) findViewById(R.id.startButton);
-        stop = (Button) findViewById(R.id.stopButton);
-        textViewTime = (TextView) findViewById(R.id.timeDisp);
-        table = (TableLayout) findViewById(R.id.tableDisp);
-        stopSoundBtn = (Button) findViewById(R.id.stopSoundButton);
 
-        //hiding
-        textViewTime.setVisibility(View.GONE);
-        stopSoundBtn.setVisibility(View.GONE);
-
-        //counter class below -> new CounterClass(time, interval)
-        // in array format due to nature of totalTime initialization
-        final CounterClass[] timer = {new CounterClass(totalTime, 1000)};
-        start.setOnClickListener(new View.OnClickListener() {
+        //start button working
+        startBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
                 if (! hoursIn.getText().toString().isEmpty())
                 {
-                    inHr = Integer.parseInt(hoursIn.getText().toString());
-                    hoursMs = hrsToMs(inHr);
+                    timeInt = Integer.parseInt(hoursIn.getText().toString());
+                    hoursMs = hrsToMs(timeInt);
                 }
                 else
                 {
@@ -107,8 +95,8 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if (! minIn.getText().toString().isEmpty())
                 {
-                    inMin = Integer.parseInt(minIn.getText().toString());
-                    minMs = minToMs(inMin);
+                    timeInt = Integer.parseInt(minIn.getText().toString());
+                    minMs = minToMs(timeInt);
                 }
                 else
                 {
@@ -122,102 +110,30 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else
                 {
-                    timer[0] = new CounterClass(totalTime, 1000);
-                    textViewTime.setVisibility(View.VISIBLE);
-                    timer[0].start();
-                    table.setVisibility(View.GONE);
+                    Intent nextPage = new Intent();
                 }
             }
         });
 
+        private int hrsToMs(int hours)
+        {
+            //site for conversions:
+            //http://amsi.org.au/teacher_modules/D0/D0g2.png
 
-        stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                table.setVisibility(View.VISIBLE);
-                textViewTime.setVisibility(View.GONE);
-                timer[0].cancel();
-            }
-        });
-
-
-    }
-
-
-    private int hrsToMs(int hours)
-    {
-        //site for conversions:
-        //http://amsi.org.au/teacher_modules/D0/D0g2.png
-
-        int minutes = hours * 60;
-        int seconds = minutes * 60;
-        int millis = seconds * 1000;
-        return  millis;
-    }
-
-    private int minToMs(int min)
-    {
-        //site for conversions:
-        //http://amsi.org.au/teacher_modules/D0/D0g2.png
-
-        int seconds = min * 60;
-        int millis = seconds * 1000;
-        return  millis;
-    }
-
-
-    public class CounterClass extends CountDownTimer {
-
-        /**
-         * @param millisInFuture    The number of millis in the future from the call
-         *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
-         *                          is called.
-         * @param countDownInterval The interval along the way to receive
-         *                          {@link #onTick(long)} callbacks.
-         */
-        public CounterClass(long millisInFuture, long countDownInterval) {
-            super(millisInFuture, countDownInterval);
+            int minutes = hours * 60;
+            int seconds = minutes * 60;
+            int millis = seconds * 1000;
+            return  millis;
         }
 
-        @Override
-        public void onTick(long millisUntilFinished) {
-            //every tick
-            long millis = millisUntilFinished;
-            String hrsminsec = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
-                    TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toMinutes(millis)),
-                    TimeUnit.MILLISECONDS.toSeconds(millis - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toSeconds(millis)))
-            );
+        private int minToMs(int min)
+        {
+            //site for conversions:
+            //http://amsi.org.au/teacher_modules/D0/D0g2.png
 
-            textViewTime.setText(hrsminsec);
-        }
-
-        @Override
-        public void onFinish() {
-
-            // Sets an ID for the notification
-            int mNotificationId = 001;
-            // Gets an instance of the NotificationManager service
-            NotificationManager mNotifyMgr =
-                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            // Builds the notification and issues it.
-            mNotifyMgr.notify(mNotificationId, mBuilder.build());
-
-            //changing visibility settings
-            table.setVisibility(View.VISIBLE);
-            stopSoundBtn.setVisibility(View.VISIBLE);
-            textViewTime.setVisibility(View.GONE);
-
-            alarmSound.start();
-
-            //gets timer to stop once button is clicked
-            //only accesible once timer finishes
-            stopSoundBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    alarmSound.stop();
-                }
-            });
-
+            int seconds = min * 60;
+            int millis = seconds * 1000;
+            return  millis;
         }
     }
 }
