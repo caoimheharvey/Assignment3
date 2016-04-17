65b04793306402a7c3d2e54999e248dfe68ba29b
caoimheharvey <caoimhe.e.harvey@gmail.com>
2016-04-17 14:50:00 +0100

    Changes to Main Activity and Timer

---
+++ app/src/main/java/dit/assignment3/Main_Activity.java
@@ -0,0 +1,25 @@
+package dit.assignment3;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/dit/assignment3/Timer.java
+++ app/src/main/java/dit/assignment3/Timer.java
@@ -1,73 +1,145 @@
 package dit.assignment3;
 
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.CountDownTimer;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.concurrent.TimeUnit;
 
-/*
- * TODO: convert bundle to long
- * TODO: play noise, AlertDialog, Notification
- */
 public class Timer extends AppCompatActivity {
 
-    //Media Player
+    //ALARM SOUND
     MediaPlayer alarmSound;
 
-    int totalTimeMs;
-    TextView timeDisp;
-    Button cancelBtn;
+    //small button minutes
+    Button ten, twenty, thirty, sixty;
+
+    Button startBtn, stopBtn;
+    EditText minIn, hoursIn;
+    int minMs, hoursMs;
+
+    //additional variables to make timer work
+    public int totalTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
+        //media
         alarmSound = MediaPlayer.create(this, R.raw.alarm_noise);
 
-        timeDisp = (TextView) findViewById(R.id.timeView);
-        cancelBtn = (Button)findViewById(R.id.cancelButton);
+       //finding by ID
+        ten = (Button) findViewById(R.id.tenMin);
+        twenty = (Button) findViewById(R.id.twentyMin);
+        thirty = (Button) findViewById(R.id.thirtyMin);
+        sixty = (Button) findViewById(R.id.sixtyMin);
 
-        Bundle time = getIntent().getExtras();
+        startBtn = (Button) findViewById(R.id.startButton);
 
-        if(time == null)
-        {
-            timeDisp.setText("Failed.");
-        }
-        else
-        {
-            timeDisp.setText("YASS");
-           // totalTimeMs = Integer.parseInt(String.valueOf(time));
-           // timeDisp.setText(totalTimeMs);
-        }
+        hoursIn = (EditText) findViewById(R.id.hoursInput);
+        minIn = (EditText) findViewById(R.id.minsInput);
 
+        //Parsing to correct value
 
-        final CounterClass timer = new CounterClass(60000, 1000);
-        timer.start();
-
-        cancelBtn.setOnClickListener(new View.OnClickListener() {
+        //changing times through smaller buttons
+        ten.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                timeDisp.setText("CANCELED");
-                timer.cancel();
-                //displays cancelled then returns to main activity
-                finish();
+                hoursIn.setText("00");
+                minIn.setText("10");
+            }
+        });
+        twenty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hoursIn.setText("00");
+                minIn.setText("20");
+            }
+        });
+        thirty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hoursIn.setText("00");
+                minIn.setText("30");
+            }
+        });
+        sixty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hoursIn.setText("01");
+                minIn.setText("00");
+            }
+        });
+
+        //start button working
+        startBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (! hoursIn.getText().toString().isEmpty())
+                {
+                    int intTime = Integer.parseInt(hoursIn.getText().toString());
+                    hoursMs = hrsToMils(intTime);
+                }
+                else
+                {
+                    hoursMs = 0;
+                }
+                if (! minIn.getText().toString().isEmpty())
+                {
+                    int intTime = Integer.parseInt(minIn.getText().toString());
+                    minMs = minToMils(intTime);
+                }
+                else
+                {
+                    minMs = 0;
+                }
+
+                totalTime = minMs + hoursMs;
+
+                if (totalTime == 0)
+                {
+                    Toast.makeText(Timer.this, "ENTER TIME", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    //start timer
+                    final CounterClass1 timer = new CounterClass1(totalTime, 1000);
+                    timer.start();
+                }
             }
         });
     }
 
+    private int hrsToMils(int hours)
+    {
+        int minutes = hours * 60;
+        int seconds = minutes * 60;
+        int millis = seconds * 1000;
+        return  millis;
+    }
 
+    private  int minToMils(int mins)
+    {
+        int seconds = mins * 60;
+        int millis = seconds * 1000;
+        return  millis;
+    }
 
-    public class CounterClass extends CountDownTimer {
+    private void nextPage()
+    {
+        Intent intent = new Intent(this, Timer.class);
+        startActivity(intent);
+    }
+
+    public class CounterClass1 extends CountDownTimer {
 
         /**
          * @param millisInFuture    The number of millis in the future from the call
@@ -76,7 +148,7 @@ public class Timer extends AppCompatActivity {
          * @param countDownInterval The interval along the way to receive
          *                          {@link #onTick(long)} callbacks.
          */
-        public CounterClass(long millisInFuture, long countDownInterval) {
+        public CounterClass1(long millisInFuture, long countDownInterval) {
             super(millisInFuture, countDownInterval);
         }
 
@@ -87,39 +159,13 @@ public class Timer extends AppCompatActivity {
                     TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished));
 
 
-            timeDisp.setText(hms);
+            //timeDisp.setText(hms);
         }
 
         @Override
         public void onFinish() {
-            timeDisp.setText("Completed.");
-            DoneAlert done = new DoneAlert();
-            //done.show();
             alarmSound.start();
         }
     }
-
-    /*
-     *  Alert Dialog
-     */
-    public class DoneAlert extends DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the Builder class for convenient dialog construction
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage("TEST")
-                    .setPositiveButton("OKAY", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // FIRE ZE MISSILES!
-                        }
-                    })
-                    .setNegativeButton("SNOOZE", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // User cancelled the dialog
-                        }
-                    });
-            // Create the AlertDialog object and return it
-            return builder.create();
-        }
-    }
 }
+
